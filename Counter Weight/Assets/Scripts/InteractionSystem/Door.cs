590289fce77132cc0052ef1aa1a5c3408911155b
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2024-05-14 19:06:09 -0700

    Adding a door to the Living Room

---
+++ Counter Weight/Assets/Scripts/InteractionSystem/Door.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace CounterWeight.InteractionSystem
+{
+    public class Door : MonoBehaviour
+    {
+        [SerializeField] private bool locked = true;
+        
+    }
+}
